79229d1568395cc213e63e1bc14e4bf499090806
domini <mikalmet@gmail.com>
2023-08-29 22:39:40 +0500

    database init

--- create_bot.py
+++ create_bot.py
@@ -1,12 +1,33 @@
+import sqlalchemy as db
 from aiogram import Dispatcher, Bot
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from os import getenv
 
-
 TOKEN = str(getenv('TOKEN'))
-print(TOKEN)
 
 storage = MemoryStorage()
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot, storage=storage)
+
+# database init
+engine = db.create_engine('sqlite:///subscribe_database.db')
+connectio = engine.connect()
+
+metadata = db.MetaData()
+
+# subscribers table
+subscribers = db.Table('subscribers', metadata,
+                       db.Column('id', db.Integer, primary_key=True),
+                       db.Column('user_id', db.Integer),
+                       db.Column('user_fullname', db.Text),
+                       db.Column('end_data', db.Date))
+
+# subscriptions table
+subscriptions = db.Table('subscriptions', metadata,
+                         db.Column('id', db.Integer, primary_key=True),
+                         db.Column('subsription_name', db.Text),
+                         db.Column('duration', db.Date),
+                         db.Column('price', db.Float))
+
+metadata.create_all(engine)
